573ff60de24215c32c76fe81b8c58d9c54c11173
Gaudencia <conny@students.uonbi.ac.ke>
2022-08-12 15:57:42 +0300

    commit tree 24aeeb7cf350e46c054c78fffe3a11fd96a68848

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves names
+  names.each_with_index do |name, index|
+    puts "#{(index +1).to_s} .#{name}"
+  end
 end
-
-def summon_captain_planet# code an argument here
+  
+  # code an argument here
   # Your code here
-end
 
-def long_planeteer_calls# code an argument here
+def summon_captain_planet captains# code an argument here
+  captains.collect do |captain|
+    "#{captain.capitalize}!"
   # Your code here
+  end
 end
+puts summon_captain_planet ["caudencia", "wanyama"]
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def long_planeteer_calls calls# code an argument here
+calls.any? do |call|
+  call.length > 4 
+  
+end
+ # Your code here
+end
+puts long_planeteer_calls(["caudencia", "wany"])
+
+def find_the_cheese cheeses# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find do |cheese|
+    
+    cheese_types.find do |cheese_type| cheese_type==cheese
+    end
+  end
+  # the array below is here to help
+  
 end
